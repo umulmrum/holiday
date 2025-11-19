a1cb811fff7e30299097af3abb0b932d602973ad
Mariusz Kaczanowski <mariusz.kaczanowski@shoper.pl>
2025-11-19 16:32:21 +0100

    Polish translations

--- src/Translator/Translator.php
+++ src/Translator/Translator.php
@@ -35,6 +35,7 @@ final class Translator implements TranslatorInterface
     {
         $this->addTranslations('en', include __DIR__ . '/../../res/trans/umulmrum_holiday.en.php');
         $this->addTranslations('de', include __DIR__ . '/../../res/trans/umulmrum_holiday.de.php');
+        $this->addTranslations('pl', include __DIR__ . '/../../res/trans/umulmrum_holiday.pl.php');
     }
 
     /**
